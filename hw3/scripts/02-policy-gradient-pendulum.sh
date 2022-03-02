3ea9b8205e32fc2464a1bccee539e936276aa0da
Simon Chamorro <simon.chamorro@usherbrooke.ca>
2022-03-02 18:42:16 -0500

    setup exp2

--- hw3/scripts/02-policy-gradient-pendulum.sh
+++ hw3/scripts/02-policy-gradient-pendulum.sh
@@ -1,4 +1,15 @@
 
 # Experiment 2
 
-python run_hw3.py --multirun env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 n_iter=100 size=64 batch_size=256,512,1024,4096 learning_rate=0.005,0.01,0.02 reward_to_go=true exp_name=q2_b<batch_size>_r<learning_rate> rl_alg=reinforce 
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=512 learning_rate=0.005 exp_name=q2_b512_r005 rl_alg=reinforce
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=1024 learning_rate=0.005 exp_name=q2_b1024_r005 rl_alg=reinforce
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=2048 learning_rate=0.005 exp_name=q2_b2048_r005 rl_alg=reinforce
+
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=512 learning_rate=0.01 exp_name=q2_b512_r01 rl_alg=reinforce
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=1024 learning_rate=0.01 exp_name=q2_b1024_r01 rl_alg=reinforce
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=2048 learning_rate=0.01 exp_name=q2_b2048_r01 rl_alg=reinforce
+
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=512 learning_rate=0.02 exp_name=q2_b512_r02 rl_alg=reinforce
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=1024 learning_rate=0.02 exp_name=q2_b1024_r02 rl_alg=reinforce
+python run_hw3.py env_name=InvertedPendulum-v2 ep_len=1000 discount=0.9 reward_to_go=true batch_size=2048 learning_rate=0.02 exp_name=q2_b2048_r02 rl_alg=reinforce
+
