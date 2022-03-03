a6ff116025cbeba6d47589c9927a3d4e6e59ab17
Simon Chamorro <simon.chamorro@usherbrooke.ca>
2022-03-02 20:16:02 -0500

    ran experiment 2 and working on plots

--- hw3/scripts/plot_exp2.py
+++ hw3/scripts/plot_exp2.py
@@ -27,7 +27,13 @@ def plot_results(data, title, i):
         x = np.arange(len(v['Eval_AverageReturn']))
         y = np.array(v['Eval_AverageReturn'])
         std = np.array(v['Eval_StdReturn'])
-        label = k.split('sb_')[-1].split('lb_')[-1].split('_Cart')[0]
+        lr = k.split('_r')[-1].split('lb_')[-1].split('_Inverted')[0]
+        if lr == '02':
+            label = 'lr: 0.02'
+        elif lr == '01':
+            label = 'lr: 0.01'
+        else:
+            label = 'lr: 0.005'
         plt.plot(x, y, label=label)
         plt.fill_between(x, y-std, y+std, alpha=0.3)
 
@@ -35,7 +41,7 @@ def plot_results(data, title, i):
     plt.xlabel('Iteration Number', fontsize=10)
     plt.title(title, fontsize=15)
     plt.legend(loc='lower right')
-    plt.savefig(plot_dir + 'q1-' + str(i) + '.png')
+    plt.savefig(plot_dir + 'q2-' + str(i) + '.png')
 
 
 if __name__ == '__main__':
@@ -44,18 +50,14 @@ if __name__ == '__main__':
     logdir = './run_logs/'
     plot_dir = './plots/'
     
-    # small batch
-    exps = glob.glob(logdir + 'hw3_q1_sb*/**/event*', recursive=True)
+    # Batch 256
+    exps = glob.glob(logdir + 'hw3_q2_b256*/**/event*', recursive=True)
     results = {}
     for exp in exps:
         results[exp.split('/')[2]] = get_eventfile_results(exp)
-    plot_results(results, 'Small Batch Experiments', '01')
+    plot_results(results, 'Batch size of 256', '0256')
 
-    # large batch
-    exps = glob.glob(logdir + 'hw3_q1_lb*/**/event*', recursive=True)
-    results = {}
-    for exp in exps:
-        results[exp.split('/')[2]] = get_eventfile_results(exp)
-    plot_results(results, 'Large Batch Experiments', '02')
+    # TODO plot aggregated results for 256 and 0.01
+
 
     
