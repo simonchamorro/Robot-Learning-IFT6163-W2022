08e75a4bb39e1129a0e21b23c8154a849655b178
Simon Chamorro <simon.chamorro@usherbrooke.ca>
2022-03-02 21:32:12 -0500

    experiment 2 plots

--- hw3/scripts/plot_exp2.py
+++ hw3/scripts/plot_exp2.py
@@ -43,6 +43,28 @@ def plot_results(data, title, i):
     plt.legend(loc='lower right')
     plt.savefig(plot_dir + 'q2-' + str(i) + '.png')
 
+def plot_agg_results(data, title):
+    fig = plt.figure()
+
+    len_train = len(next(iter(data.values()))['Eval_AverageReturn'])
+    all_runs = np.zeros((len(data.keys()), len_train)) 
+    i = 0
+    for k, v in data.items():
+        all_runs[i,:] = np.array(v['Eval_AverageReturn'])
+        i += 1
+    x = np.arange(len(v['Eval_AverageReturn']))
+    y = np.mean(all_runs, axis=0)
+    std = np.std(all_runs, axis=0)
+    label = 'b=256, lr=0.01'
+
+    plt.plot(x, y, label=label)
+    plt.fill_between(x, y-std, y+std, alpha=0.3)
+
+    plt.ylabel('Average Reward', fontsize=10)
+    plt.xlabel('Iteration Number', fontsize=10)
+    plt.title(title, fontsize=15)
+    plt.legend(loc='lower right')
+    plt.savefig(plot_dir + 'q2-final.png')
 
 if __name__ == '__main__':
     import glob
@@ -57,7 +79,12 @@ if __name__ == '__main__':
         results[exp.split('/')[2]] = get_eventfile_results(exp)
     plot_results(results, 'Batch size of 256', '0256')
 
-    # TODO plot aggregated results for 256 and 0.01
+    # Plot aggregated results for 256 and 0.01
+    exps = glob.glob(logdir + 'hw3_q2_final*/**/event*', recursive=True)
+    results = {}
+    for exp in exps:
+        results[exp.split('/')[2]] = get_eventfile_results(exp)
+    plot_agg_results(results, 'Final Config Over 5 Seeds')
 
 
     
